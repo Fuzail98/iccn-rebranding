e5125579841f6c5461eb5999d78962c311a62ad6
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-05-30 17:50:57 +0530

    Update core_AGW.sh

--- core_AGW.sh
+++ core_AGW.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-vi /usr/local/opnsense/version/core
+cd /usr/local/opnsense/version/
+pkg install jq
 
-if [ ICCN in core ]
-then
-    echo "Already there!!"
-else
-    echo "Please change!!!"
+jq '.product_copyright_owner = "ICCN"' core > tmp.json
+jq '.product_copyright_url = "https://www.iccnetworking.com"' core > tmp.json
+jq '.product_name = "AGW"' core > tmp.json
+jq '.product_nickname = "AGW"' core > tmp.json
+jq '.product_website = "https://www.iccnetworking.com"' core > tmp.json
+
+cp tmp.json core
