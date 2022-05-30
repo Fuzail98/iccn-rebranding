d939845f531a7f1f31b84988cc4d29d716398309
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-05-30 18:03:23 +0530

    Update core_AGW.sh

--- core_AGW.sh
+++ core_AGW.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 cd /usr/local/opnsense/version/
-pkg install jq
+out=pkg install jq
+if "The most recent versions of packages are already installed" in $out:
+then
+  echo "!..!"
+else
+  echo "Installing package 'jq'"
 
-jq '.product_copyright_owner = "ICCN"' core > tmp.json
-jq '.product_copyright_url = "https://www.iccnetworking.com"' core > tmp.json
-jq '.product_email = "info@iccnetworking.com"' core > tmp.json
-jq '.product_name = "AGW"' core > tmp.json
-jq '.product_nickname = "AGW"' core > tmp.json
-jq '.product_website = "https://www.iccnetworking.com"' core > tmp.json
+jq '.product_copyright_owner = "ICCN"' core > temp.json
+jq '.product_copyright_url = "https://www.iccnetworking.com"' core > temp.json
+jq '.product_email = "info@iccnetworking.com"' core > temp.json
+jq '.product_name = "AGW"' core > temp.json
+jq '.product_nickname = "AGW"' core > temp.json
+jq '.product_website = "https://www.iccnetworking.com"' core > temp.json
 
-cp tmp.json core
+cp temp.json core
 
-rm tmp.json
+rm temp.json
