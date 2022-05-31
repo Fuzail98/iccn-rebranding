0dd980d98da1ceed908a0db5678d6ea25cdbae42
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-05-31 17:22:15 +0530

    Update core_AGW.sh

--- core_AGW.sh
+++ core_AGW.sh
@@ -20,11 +20,23 @@ fi
 # Making changes in the JSON file
 
 jq '.product_copyright_owner = "ICCN"' core > temp.json
+cp temp.json core
+rm temp.json
 jq '.product_copyright_url = "https://www.iccnetworking.com"' core > temp.json
+cp temp.json core
+rm temp.json
 jq '.product_email = "info@iccnetworking.com"' core > temp.json
+cp temp.json core
+rm temp.json
 jq '.product_name = "AGW"' core > temp.json
+cp temp.json core
+rm temp.json
 jq '.product_nickname = "AGW"' core > temp.json
+cp temp.json core
+rm temp.json
 jq '.product_website = "https://www.iccnetworking.com"' core > temp.json
+cp temp.json core
+rm temp.json
 
 cp temp.json core
 rm temp.json
