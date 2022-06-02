4bb734f14a9362bf11e3812cbca6c46440fdc243
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-06-02 14:49:12 +0530

    Update iccn_complete.sh

--- iccn_complete.sh
+++ iccn_complete.sh
@@ -25,10 +25,10 @@ cd /usr/local/opnsense/version/
 # Since the core file is json data, we will be needing the 'jq' package
 
 pkg_name=jq
-out=$(pkg info $pkg_name | grep Name)
+out1=$(pkg info $pkg_name | grep Name)
 compare_string="Name           : $pkg_name"
 
-if [ "$out" == "$compare_string" ]
+if [ "$out1" == "$compare_string" ]
 then
     echo "Package: $pkg_name already installed. Executing the commands to make changes in the core file!!!"
 else
@@ -45,4 +45,13 @@ jq '.product_name = "AGW"' core > test.json && mv test.json core
 jq '.product_nickname = "AGW"' core > test.json && mv test.json core
 jq '.product_website = "https://www.iccnetworking.com"' core > test.json && mv test.json core
 
-echo "Changes have been made in the core file!!! Please refresh and/or clear cache on your browser to see the changes."
+out2=$(jq '.product_copyright_owner' /usr/local/opnsense/version/core)
+# echo $out2
+
+if [ "$out2" == '"ICCN"' ]
+then
+        echo "Changes have been made in the core file!!! Please refresh and/or clear cache on your browser to see the changes."
+else
+        echo "Changes in the core file have not been made. Please try again."
+fi
+
