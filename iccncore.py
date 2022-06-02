c935dcb8b131dc03ae79d72b12ce6088d611096a
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-06-02 11:37:19 +0530

    Update iccncore.py

--- iccncore.py
+++ iccncore.py
@@ -13,7 +13,7 @@ ipaddr = input('Enter the IP address of the gateway: ')
 usrnm = input('Enter the username with root privileges: ')
 pswd = getpass.getpass('Enter the password: ')
 
-print(f'Connecting to {ipaddr}...')
+# print(f'Connecting to {ipaddr}...')
 sshcli.connect(hostname=ipaddr, port=22, username=usrnm, password=pswd, look_for_keys=False, allow_agent=False)
 time.sleep(2)
 
@@ -30,7 +30,7 @@ time.sleep(1)
 ###############################################################################################################
 # Installing required packages if not installed
 
-packages = ('bash', 'jq', 'wget')
+packages = ['bash', 'jq', 'wget']
 for pkg in packages:
     shell.send(f'pkg info {pkg}' + '\n')
     time.sleep(2)
@@ -43,32 +43,32 @@ for pkg in packages:
         print(f'{pkg} has been installed!')
     else:
         print(f'{pkg} already installed')
-tmpout3 = shell.recv(10000)
 
 ###############################################################################################################
 # Running the commands for changes to take place
 
 with open('core_commands.txt') as f:
     commands = f.read().splitlines()
-
-print(f'Changes taking place on {ipaddr}, Please wait...')
+time.sleep(2)
+# print(f'Changes taking place on {ipaddr}, Please wait...')
+print('changes taking places')
 for cmnd in commands:
-    # print(f'Sending command: {cmnd}')
+    print(f'Sending command: {cmnd}')
     shell.send(cmnd + '\n')
-    time.sleep(4)
-tmpout4 = shell.recv(10000)
+    time.sleep(2)
+tmpout3 = shell.recv(10000)
 
 ###############################################################################################################
 # Conditional output to check if the changes have taken place
 
 shell.send("jq '' /usr/local/opnsense/version/core | grep ICCN" + '\n')
 time.sleep(2)
-tmpout5 = shell.recv(10000)
-# print(tmpout5.decode())
+tmpout4 = shell.recv(10000)
+# print(tmpout4.decode())
 
-print('#' * 100)
+print('#' * 120)
 
-if 'ICCN' in tmpout5.decode():
+if 'ICCN' in tmpout4.decode():
     print('Changes have been changed! Please refresh your GUI and/or clear the cache on your browser.')
 else:
     print("Changes haven't been made!!! Please try again.")
