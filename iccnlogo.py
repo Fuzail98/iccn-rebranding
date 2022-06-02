a1417e3e88020483691fcb10dc8f27cc5af3e5d9
Fuzail98 <102956978+Fuzail98@users.noreply.github.com>
2022-06-02 14:37:41 +0530

    Update iccnlogo.py

--- iccnlogo.py
+++ iccnlogo.py
@@ -6,46 +6,72 @@ sshcli = paramiko.SSHClient()
 sshcli.load_system_host_keys()
 sshcli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ipaddr = input('Enter the IP address of the host: ')
-usrnm = input('Enter user with root privileges: ')
-pswd = getpass.getpass('Enter the root password: ')
+###############################################################################################################
+# Establishing SSH connection with the gateway
+
+ipaddr = input('Enter the IP address of the gateway: ')
+usrnm = input('Enter the username with root privileges: ')
+pswd = getpass.getpass('Enter the password: ')
 
 print(f'Connecting to {ipaddr}...')
-sshcli.connect(hostname=ipaddr, port=22, username='root', password=pswd, look_for_keys=False, allow_agent=False)
+sshcli.connect(hostname=ipaddr, port=22, username=usrnm, password=pswd, look_for_keys=False, allow_agent=False)
+time.sleep(2)
+
+###############################################################################################################
+# Invoking shell
 
 shell = sshcli.invoke_shell()
-print('Connected!!!')
 shell.send('8' + '\n')
-time.sleep(1)
-tmpout1 = shell.recv(10000)
-print('\n')
-
-# Checking if git package is installed and install it if it isn't
-
-shell.send(f'pkg info git' + '\n')
 time.sleep(2)
-tmpout2 = shell.recv(10000)
-if "pkg: No package(s) matching git" in tmpout2.decode():
-    shell.send('pkg install git' + '\n')
-    time.sleep(2)
-    shell.send('y' + '\n')
-    time.sleep(2)
-    print('git has been installed!')
-else:
-    print('git already installed')
+tmpout1 = shell.recv(10000)
+print('Connected!!!')
+time.sleep(1)
 
-with open('commands.txt') as f:
+###############################################################################################################
+# Installing required packages if not installed
+
+packages = ['bash', 'jq', 'wget', 'git']
+for pkg in packages:
+    shell.send(f'pkg info {pkg}' + '\n')
+    time.sleep(2)
+    tmpout2 = shell.recv(10000)
+    if f"pkg: No package(s) matching {pkg}" in tmpout2.decode():
+        shell.send(f'pkg install {pkg}' + '\n')
+        time.sleep(2)
+        shell.send('y' + '\n')
+        time.sleep(2)
+        print(f'{pkg} has been installed!')
+    else:
+        print(f'{pkg} already installed')
+
+###############################################################################################################
+# Running the commands for changes to take place
+
+with open('commands_core_logo.txt') as f:
     commands = f.read().splitlines()
-
-print(f'Changes taking place on {ipaddr}, Please wait...')
+time.sleep(2)
+# print(f'Changes taking place on {ipaddr}, Please wait...')
+print('changes taking places')
 for cmnd in commands:
-    # print(f'Sending command: {cmnd}')
+    print(f'Sending command: {cmnd}')
     shell.send(cmnd + '\n')
     time.sleep(2)
-
 tmpout3 = shell.recv(10000)
 
-print('#' * 100)
+print('#' * 120)
+
+###############################################################################################################
+# Conditional output to check if the changes have taken place
+
+shell.send("jq '' /usr/local/opnsense/version/core | grep ICCN" + '\n')
+time.sleep(2)
+tmpout4 = shell.recv(10000)
+# print(tmpout4.decode())
+
+if 'ICCN' in tmpout4.decode():
+    print('Changes have been changed! Please refresh your GUI and/or clear the cache on your browser.')
+else:
+    print("Changes haven't been made!!! Please try again.")
 
 shell.send('ls /usr/local/opnsense/www/themes/tukan/build/images/' + '\n')
 time.sleep(2)
@@ -56,6 +82,9 @@ if 'iccn' in tmpout4.decode():
     print('Changes have been changed! Please refresh your GUI and/or clear the cache on your browser.')
 else:
     print("Changes haven't been made!!! Please try again.")
+
+###############################################################################################################
+# Closing SSH connection with the gateway
 
 if sshcli.get_transport().is_active():
     print('Closing connection...')
